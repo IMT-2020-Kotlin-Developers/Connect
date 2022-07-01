7da791a02914657f7c4347fdad4e04416ed07d17
addy0110 <addycoder0110@gmail.com>
2022-07-01 12:33:28 +0530

    Added Log In and Sign In page

--- app/src/main/java/com/example/connect/IntroScreenFragment.kt
+++ app/src/main/java/com/example/connect/IntroScreenFragment.kt
@@ -5,14 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.connect.databinding.FragmentIntroScreenBinding
 
 class IntroScreenFragment : Fragment() {
 
+    lateinit var binding: FragmentIntroScreenBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_intro_screen, container, false)
+    ): View {
+
+        binding = FragmentIntroScreenBinding.inflate(layoutInflater, container, false)
+        binding.btLogIn.setOnClickListener{
+
+        }
+        binding.btSignIn.setOnClickListener{
+            findNavController().navigate(R.id.action_introScreenFragment_to_signInFragment)
+        }
+        binding.btLogIn.setOnClickListener{
+            findNavController().navigate(R.id.action_introScreenFragment_to_logInFragment)
+        }
+        return binding.root
     }
 
 }
